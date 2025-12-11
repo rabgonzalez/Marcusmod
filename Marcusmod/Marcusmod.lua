69ed947fad28da17520e141b07ee67105b3ce742
rabgonzalez <rubalba.rag@gmail.com>
2025-12-11 09:43:37 +0000

    styles(modicon): modicon asset changed

--- Marcusmod/Marcusmod.lua
+++ Marcusmod/Marcusmod.lua
@@ -1,4 +1,11 @@
-Marcusmod = {}
+if SMODS.Atlas then
+    SMODS.Atlas({
+        key = "modicon",
+        path = "modicon.png",
+        px = 32,
+        py = 32
+    })
+end
 
 -- Dinamic loaded Jokers
 local joker_src = NFS.getDirectoryItems(SMODS.current_mod.path .. "jokers")
